4b750371e6a99dbd9b8eda613ccaabeebf91b397
levdok <evdokimova.l@hotmail.com>
2020-07-09 18:05:03 +0300

    fixed

--- timesheet.py
+++ timesheet.py
@@ -1,5 +1,5 @@
 import re
-from configparser import SafeConfigParser
+from ConfigParser import SafeConfigParser
 
 import arrow
 import pandas as pd
@@ -11,12 +11,12 @@ TEMPO_URL = 'https://api.tempo.io/core/3/worklogs'
 JIRA_REGEXP = re.compile(r'[A-Za-z]{2,5}-\d+')
 END_TIME = arrow.utcnow().to('Europe/Moscow').format('YYYY-MM-DDTHH:mm:ssZZ')
 
-CONFIG = SafeConfigParser()
-CONFIG.read(SETTINGS_FILE)
+config = SafeConfigParser()
+config.read(SETTINGS_FILE)
 
-params = {'start_date': CONFIG.get('tempo', 'lastSuccessful'), 'end_date': END_TIME}
+params = {'start_date': config.get('tempo', 'lastSuccessful'), 'end_date': END_TIME}
 
-toggl_response = requests.get(TOGGL_URL, auth=(CONFIG.get('toggl', 'token'), 'api_token'), params=params)
+toggl_response = requests.get(TOGGL_URL, auth=(config.get('toggl', 'token'), 'api_token'), params=params)
 
 df = pd.DataFrame(toggl_response.json())
 df['date'] = df.apply(lambda row: (arrow.get(row.start)).date().isoformat(), axis=1)
@@ -28,13 +28,13 @@ for task in collapsed_data:
     if JIRA_REGEXP.search(task.get('description')):
         key = JIRA_REGEXP.search(task.get('description')).group()
     else:
-        key = CONFIG.get('tempo', 'defaultKey')
+        key = config.get('tempo', 'defaultKey')
 
     start_date = task.get('date')
     start_time = arrow.get(task.get('start')).time().isoformat()
     description = ''
     if not description:
-        description = CONFIG.get('tempo', 'defaultDescription')
+        description = config.get('tempo', 'defaultDescription')
     else:
         description = task.get('description').replace(key, '').strip()
 
@@ -44,16 +44,16 @@ for task in collapsed_data:
         'startDate': start_date,
         'startTime': start_time,
         'description': description,
-        'authorAccountId': CONFIG.get('authorAccountId'),
+        'authorAccountId': config.get('tempo', 'authorAccountId')
     }
 
     requests.post(
-        TEMPO_URL, json=data, headers={'Content-Type': 'application/json', 'Authorization': 'Bearer ' + CONFIG.get('tempo', 'token')}
+        TEMPO_URL, json=data, headers={'Content-Type': 'application/json', 'Authorization': 'Bearer ' + config.get('tempo', 'token')}
     )
 
-CONFIG.set('tempo', 'lastSuccessful', END_TIME)
+config.set('tempo', 'lastSuccessful', END_TIME)
 
-    with open('settings.ini', 'wb') as file:
-    CONFIG.write(file)
+with open('settings.ini', 'wb') as file:
+    config.write(file)
 
 print('done')
